244d8d44e86401ea3fb736c503b6151b49a57f7c
Jared Jubas-Malz <jared.jubas.malz@gmail.com>
2023-04-13 16:46:53 -0400

    rotate caret when dropdown clicked, add message if no cities found, and make dropdown list responsive for mobile

--- src/Components/LocationList/LocationList.jsx
+++ src/Components/LocationList/LocationList.jsx
@@ -3,54 +3,63 @@ import './LocationList.css';
 import CitiesList from '../CitiesList/CitiesList';
 
 const showCities = (event) => {
+  // TODO is there an easier way to access the elements?
   const parent = event.target.parentElement;
   const currentDisplay = parent.parentElement.lastChild.style.display;
 
-  // const caretDown = parent.children[2];
-  // const caretUp = parent.children[3];
+  const caret = parent.children[2];
 
   if (currentDisplay === 'block') {
+    // Hide the dropdown
     parent.parentElement.lastChild.style.display = 'none';
 
-    // caretUp.style.display = 'none';
-    // caretDown.style.display = 'block';
+    // Rotate caret to point down
+    caret.style.transform = 'rotate(0deg)';
   } else {
+    // Show the dropdown
     parent.parentElement.lastChild.style.display = 'block';
 
-    // caretDown.style.display = 'none';
-    // caretUp.style.display = 'block';
+    // Rotate caret to point up
+    caret.style.transform = 'rotate(180deg)';
   }
 };
 
 const LocationList = ({ locations }) => {
+  // Get a sorted list of countries so we can display it in alphabetical order
   const countries = Object.entries(locations).sort();
+
   const totalCities = countries.reduce((acc, curr) => {
     return acc + curr[1].length;
   }, 0);
 
   return (
     <div className="results-container">
-      <div className="results-header">
-        Found the following {totalCities} cities in {countries.length}{' '}
-        countries. <br></br> Click each dropdown to view the cities in that
-        country!
-      </div>
+      {totalCities > 0 ? (
+        <div className="results-header">
+          Found the following {totalCities} cities in {countries.length}{' '}
+          countries. <br></br> Click each dropdown to view the cities in that
+          country!
+        </div>
+      ) : (
+        <div className="results-header">
+          No cities found. Try changing the inputs and making another search!
+        </div>
+      )}
       <div className="countries-container">
         {countries.map(([country, cities]) => {
           const numCities = cities.length;
           const numCitiesText = numCities === 1 ? 'City' : 'Cities';
           return (
-            <div
-              className="country-dropdown-container"
-              key={country}
-              onClick={(event) => showCities(event)}
-            >
-              <div className="country-dropdown">
+            <div className="country-dropdown-container" key={country}>
+              <div
+                className="country-dropdown"
+                onClick={(event) => showCities(event)}
+              >
                 <div className="country">{country}</div>
                 <div className="num-cities">
                   {numCities} {numCitiesText}
                 </div>
-                <div className="caret-down">
+                <div className="caret">
                   <div className="caret-left"></div>
                   <div className="caret-right"></div>
                 </div>
--- src/Components/VacationFinder/VacationFinder.jsx
+++ src/Components/VacationFinder/VacationFinder.jsx
@@ -58,64 +58,66 @@ const VacationFinder = () => {
       <Navbar />
 
       <div className="vacation-finder-container">
-        {firstTime && (
-          <p className="initial-message">
-            Search for vacation destinations that works for you!
-          </p>
-        )}
-        <ErrorMessage />
-        <div className="search-container">
-          <div className="search-components">
-            <div className="min-temp-container">
-              <label className="min-temp" htmlFor="minTemp">
-                Minimum Temperature
-              </label>
-              <input
-                type="text"
-                className="min-temp-input"
-                name="minTemp"
-                value={minTemp}
-                onChange={handleMinTemp}
-              ></input>
-            </div>
-            <div className="max-temp-container">
-              <label className="max-temp" htmlFor="maxTemp">
-                Maximum Temperature
-              </label>
-              <input
-                type="text"
-                className="max-temp-input"
-                name="maxTemp"
-                value={maxTemp}
-                onChange={handleMaxTemp}
-              ></input>
-            </div>
-            <div className="month-container">
-              <select className="month" id="month" required>
-                <option value="" disabled selected>
-                  Select a month
-                </option>
-                <option value="jan">January</option>
-                <option value="feb">February</option>
-                <option value="mar">March</option>
-                <option value="apr">April</option>
-                <option value="may">May</option>
-                <option value="jun">June</option>
-                <option value="jul">July</option>
-                <option value="aug">August</option>
-                <option value="sep">September</option>
-                <option value="oct">October</option>
-                <option value="nov">November</option>
-                <option value="dec">December</option>
-              </select>
-            </div>
-            <div className="submit-container">
-              <button
-                onClick={() => getData(minTemp, maxTemp)}
-                className="submit-button"
-              >
-                Search
-              </button>
+        <div className="search-bar-container">
+          {firstTime && (
+            <p className="initial-message">
+              Search for vacation destinations that works for you!
+            </p>
+          )}
+          <ErrorMessage />
+          <div className="search-container">
+            <div className="search-components">
+              <div className="min-temp-container">
+                <label className="min-temp" htmlFor="minTemp">
+                  Minimum Temperature
+                </label>
+                <input
+                  type="text"
+                  className="min-temp-input"
+                  name="minTemp"
+                  value={minTemp}
+                  onChange={handleMinTemp}
+                ></input>
+              </div>
+              <div className="max-temp-container">
+                <label className="max-temp" htmlFor="maxTemp">
+                  Maximum Temperature
+                </label>
+                <input
+                  type="text"
+                  className="max-temp-input"
+                  name="maxTemp"
+                  value={maxTemp}
+                  onChange={handleMaxTemp}
+                ></input>
+              </div>
+              <div className="month-container">
+                <select className="month" id="month" required>
+                  <option value="" disabled selected>
+                    Select a month
+                  </option>
+                  <option value="jan">January</option>
+                  <option value="feb">February</option>
+                  <option value="mar">March</option>
+                  <option value="apr">April</option>
+                  <option value="may">May</option>
+                  <option value="jun">June</option>
+                  <option value="jul">July</option>
+                  <option value="aug">August</option>
+                  <option value="sep">September</option>
+                  <option value="oct">October</option>
+                  <option value="nov">November</option>
+                  <option value="dec">December</option>
+                </select>
+              </div>
+              <div className="submit-container">
+                <button
+                  onClick={() => getData(minTemp, maxTemp)}
+                  className="submit-button"
+                >
+                  Search
+                </button>
+              </div>
             </div>
           </div>
         </div>
