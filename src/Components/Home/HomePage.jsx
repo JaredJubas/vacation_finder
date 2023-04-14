7c97dcbcc576633587f05a5cf2296f23699fcef5
Jared Jubas-Malz <jared.jubas.malz@gmail.com>
2023-04-14 13:34:57 -0400

    add home page styling

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,8 +1,24 @@
 import './Homepage.css';
+import Navbar from '../Navbar/Navbar';
 
 const HomePage = () => {
   return (
-    <div className="home-container">This will eventually be a home page</div>
+    <div className="home-page-container">
+      <Navbar />
+      <div className="home-container">
+        <header className="home-header">
+          <div className="header-container">
+            <h1 className="title">Vacation Finder</h1>
+            <p className="subtitle">Vacation finder tool</p>
+            <div className="vacation-finder-button-container">
+              <a className="vacation-finder-button" href="/vacationFinder">
+                Get started!
+              </a>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
   );
 };
 
