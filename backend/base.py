6f278abc3ab55dcc80aba2f62736191ef0366c29
Jared Jubas-Malz <jared.jubas.malz@gmail.com>
2023-04-17 20:34:53 -0400

    enable cors for APIs

--- backend/base.py
+++ backend/base.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from get_cities import get_cities
 from get_database import get_database
+from flask_cors import CORS
 
 api = Flask(__name__)
+CORS(api)
 
 
 @api.route('/cities')
