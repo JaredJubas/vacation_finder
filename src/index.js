a68444fa4ea49e17c829d79d924b6ee5e956915b
Jared Jubas-Malz <jared.jubas.malz@gmail.com>
2023-04-17 20:19:55 -0400

    add basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import HomePage from './Components/Home/HomePage';
 import VacationFinder from './Components/VacationFinder/VacationFinder';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/vacationFinder" element={<VacationFinder />} />
