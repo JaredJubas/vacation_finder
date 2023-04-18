820dd2a2dad4c65aca34b4b4bdd9e25886237f7b
Jared Jubas-Malz <jared.jubas.malz@gmail.com>
2023-04-17 20:44:43 -0400

    explicitly include content-type in cors header

--- backend/base.py
+++ backend/base.py
@@ -4,7 +4,7 @@ from get_database import get_database
 from flask_cors import CORS
 
 api = Flask(__name__)
-CORS(api)
+CORS(api, resources={r"/*": {"origins": "*"}}, headers=['Content-Type'])
 
 
 @api.route('/cities')
